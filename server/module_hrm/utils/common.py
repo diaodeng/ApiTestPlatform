849de1adc4b25cd93089178701e23d7b7f60e142
xj <wwwxiongjie@126.com>
2024-07-24 19:01:00 +0800

    1.key_value_dict小优化

--- server/module_hrm/utils/common.py
+++ server/module_hrm/utils/common.py
@@ -86,7 +86,7 @@ def key_value_list(keyword, cover_list: list):
     return lists
 
 
-def key_value_dict(cover_list: list[dict]|dict, ignore_type=False):
+def key_value_dict(cover_list: list[dict] | dict, ignore_type=False):
     """
     根据数据类型处理字典数据,如果参数cover_list不是列表则原样返回
     [{"key": "key1", "value": "value1", "type": "sting"}，{"key": "key2", "value": "value2", "type": "int"}] => {"key1": "value1", "key2": value2}
@@ -99,7 +99,7 @@ def key_value_dict(cover_list: list[dict]|dict, ignore_type=False):
         return cover_list
     dicts = {}
     for value in cover_list:
-
+        if not value.pop("enable", False): continue
         key = value.pop('key')
         if not key:
             continue
@@ -107,9 +107,8 @@ def key_value_dict(cover_list: list[dict]|dict, ignore_type=False):
         if not ignore_type:
             type = value.pop("type", "str")
             val = type_change(type, val)
-        enable = value.pop("enable", False)
-        if enable:
-            dicts[key] = val
+
+        dicts[key] = val
     return dicts
 
 
@@ -133,7 +132,3 @@ def ensure_path(path_str: str):
     path_str = path_str.replace('\\', os.sep)
     path_str = path_str.replace("/", os.sep)
     return path_str
-
-
-
-
