f1928ef779e5a4d9001e763edb7e6f855892e093
jing.tang <tj569984165@qq.com>
2024-07-31 20:21:21 +0800

    设置debugtalk 列表也支持按项目、状态检索

--- server/module_hrm/dao/debugtalk_dao.py
+++ server/module_hrm/dao/debugtalk_dao.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from module_hrm.entity.do.debugtalk_do import HrmDebugTalk
+from module_hrm.entity.do.project_do import HrmProject
 from module_hrm.entity.vo.debugtalk_vo import *
 
 
@@ -16,7 +17,7 @@ class DebugTalkDao:
         :param debugtalk_id: DebugTalkid
         :return: 在用DebugTalk信息对象
         """
-        debugtalk_info = db.query(HrmDebugTalk)\
+        debugtalk_info = db.query(HrmDebugTalk) \
             .filter(HrmDebugTalk.debugtalk_id == debugtalk_id,
                     HrmDebugTalk.status == 0,
                     HrmDebugTalk.del_flag == 0) \
@@ -25,14 +26,21 @@ class DebugTalkDao:
         return debugtalk_info
 
     @classmethod
-    def get_debugtalk_list(cls, db: Session):
+    def get_debugtalk_list(cls, db: Session, page_object: DebugTalkModel, data_scope_sql: str):
         """
         用于获取DebugTalk列表的工具方法
         :param db: orm对象
         :return: DebugTalk的信息对象
         """
-        debugtalk_list = db.query(HrmDebugTalk).filter(HrmDebugTalk.del_flag == 0).all()
-
+        debugtalk_list = (db.query(HrmDebugTalk).join(HrmProject, HrmDebugTalk.project_id == HrmProject.project_id).
+                          filter(HrmDebugTalk.del_flag == 0,
+                                 HrmDebugTalk.status == page_object.status if page_object.status else True,
+                                 HrmDebugTalk.project_id == page_object.project_id if page_object.project_id else True,
+                                 eval(data_scope_sql)).order_by(HrmDebugTalk.debugtalk_id).all())
+        debugtalk_list = [{"debugtalk_id": obj.debugtalk_id, "project_id": obj.project_id,
+                           "debugtalk": obj.debugtalk, "status": obj.status,
+                           "project_name": obj.project.project_name, "create_by": obj.create_by,
+                           "update_by": obj.update_by, "create_time": obj.create_time, "update_time": obj.update_time} for obj in debugtalk_list]
         return debugtalk_list
 
     @classmethod
@@ -49,7 +57,6 @@ class DebugTalkDao:
             .first()
 
         return debugtalk_info
-
 
     @classmethod
     def add_debugtalk_dao(cls, db: Session, debugtalk: DebugTalkModel):
@@ -87,4 +94,5 @@ class DebugTalkDao:
         """
         db.query(HrmDebugTalk) \
             .filter(HrmDebugTalk.project_id == debugtalk.project_id) \
-            .update({HrmDebugTalk.del_flag: '2', HrmDebugTalk.update_by: debugtalk.update_by, HrmDebugTalk.update_time: debugtalk.update_time})
+            .update({HrmDebugTalk.del_flag: '2', HrmDebugTalk.update_by: debugtalk.update_by,
+                     HrmDebugTalk.update_time: debugtalk.update_time})
