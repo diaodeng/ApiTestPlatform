592e8b0b03db1d5592b3d048859da49ec4887ee3
jing.tang <tj569984165@qq.com>
2024-07-31 20:21:21 +0800

    设置debugtalk 列表也支持按项目、状态检索

--- server/module_hrm/entity/do/debugtalk_do.py
+++ server/module_hrm/entity/do/debugtalk_do.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, Text, ForeignKey, BigInteger
+from sqlalchemy.orm import relationship
 
 from config.database import Base
 from module_hrm.entity.do.common_do import BaseModel
@@ -7,7 +8,7 @@ from utils.snowflake import snowIdWorker
 
 class HrmDebugTalk(Base, BaseModel):
     """
-    用户信息表
+    DebugTalk信息表
     """
     class Meta:
         verbose_name = '驱动py文件'
@@ -19,11 +20,11 @@ class HrmDebugTalk(Base, BaseModel):
     project_id = Column(BigInteger, ForeignKey('hrm_project.project_id'), nullable=False)
 
     # 引用 Project，表示 DebugTalk 属于哪个 Project
-    # project = relationship("HrmProject", back_populates="hrm_debugtalk")
+    project = relationship("HrmProject", back_populates="hrm_debugtalk")
 
     debugtalk = Column(Text(collation='utf8_general_ci'), nullable=True, comment='#debugtalk.py')
     status = Column(String(1, collation='utf8_general_ci'), default='0', comment='状态（0正常 1停用）')
     del_flag = Column(String(1, collation='utf8_general_ci'), default='0', comment='删除标志（0代表存在 2代表删除）')
 
-    # def __repr__(self):
-    #     return f"<{self.project.project_name})>"
+    def __repr__(self):
+        return f"<{self.project.project_name})>"
