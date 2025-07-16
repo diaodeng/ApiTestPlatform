65bf8b375f3da4b4acd887a95221630708eada98
jie.xiong <jie.xiong@dmall.com>
2025-07-16 10:03:40 +0800

    增加报告导出为html，修复任务状态修改失败bug

--- server/start.sh
+++ server/start.sh
@@ -3,7 +3,7 @@
 echo "开始安装系统依赖。。。"
 sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list && \
     sed -i 's|security.debian.org/debian-security|mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list && \
-    apt-get update && apt-get install --no-install-recommends -y nodejs npm && \
+    apt-get update && apt-get install --no-install-recommends -y nodejs npm wkhtmltopdf && \
     npm install --registry=https://registry.npmmirror.com jsonpath && \
     npm install --registry=https://registry.npmmirror.com jmespath
 
