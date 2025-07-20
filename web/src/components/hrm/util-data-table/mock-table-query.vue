060ccf4783b36719c4ef145efc0ee9579611e084
xiongjie <wwwxiongjie@gmail.com>
2025-07-20 08:29:01 +0800

    新功能，mock功能开发

---
+++ web/src/components/hrm/util-data-table/mock-table-query.vue
@@ -0,0 +1,344 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch">
+      <el-form-item label="所属项目" prop="projectId">
+        <el-select v-model="queryParams.projectId" placeholder="请选择" @change="resetModule" clearable filterable
+                   style="width: 150px">
+          <el-option
+              v-for="option in projectOptions"
+              :key="option.projectId"
+              :label="option.projectName"
+              :value="option.projectId">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="所属模块" prop="moduleId">
+        <el-select v-model="queryParams.moduleId" placeholder="请选择" clearable filterable style="width: 150px">
+          <el-option
+              v-for="option in moduleOptions"
+              :key="option.moduleId"
+              :label="option.moduleName"
+              :value="option.moduleId">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item :label="dataName+'ID'" prop="ruleId">
+        <el-input
+            v-model="queryParams.ruleId"
+            :placeholder="'请输入'+dataName+'ID'"
+            clearable
+            style="width: 200px"
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="path" prop="path">
+        <el-input
+            v-model="queryParams.path"
+            placeholder="'请输入path"
+            clearable
+            style="width: 200px"
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item :label="dataName+'名称'" prop="name">
+        <el-input
+            v-model="queryParams.name"
+            :placeholder="'请输入'+dataName+'名称'"
+            clearable
+            style="width: 200px"
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="状态" prop="status">
+        <el-select v-model="queryParams.status" :placeholder="dataName+'状态'" clearable style="width: 100px">
+          <el-option
+              v-for="dict in qtr_case_status"
+              :key="dict.value * 1"
+              :label="dict.label"
+              :value="dict.value * 1"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-checkbox v-model="onlySelf" @change="handleQuery">仅自己的数据</el-checkbox>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery" :loading="loading.page" :disabled="loading.page">
+          搜索
+        </el-button>
+        <el-button type="default" icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <slot name="table-tool"></slot>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading.page"
+              ref="tableRef"
+              :data="ruleList"
+              @select="handleSelect"
+              @select-all="handleSelectAll"
+              @selection-change="handleSelectionChange"
+              border
+              table-layout="fixed"
+              max-height="calc(100vh - 280px)"
+    >
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column :label="dataName+'ID'" prop="ruleId" width="150px"/>
+      <el-table-column :label="dataName+'名称'" prop="name" width="auto" min-width="200px"/>
+      <el-table-column label="所属项目" prop="projectName">
+        <template #default="scope">
+          <span>{{ nameOrGlob(scope.row.projectName) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="所属模块" prop="moduleName">
+        <template #default="scope">
+          <span>{{ nameOrGlob(scope.row.moduleName) }}</span>
+        </template>
+      </el-table-column>
+      <!--         <el-table-column label="用例排序" align="center" prop="sort" />-->
+      <el-table-column label="状态" align="center" prop="status" width="120px">
+        <template #default="scope">
+          <slot name="caseStatus" :scope="scope">
+            <DictTag :options="qtr_case_status" :value="scope.row.status"></DictTag>
+          </slot>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建人" align="center" prop="createBy" width="80px"></el-table-column>
+      <el-table-column label="创建时间" align="center" prop="createTime" class-name="small-padding fixed-width"
+                       width="150px">
+        <template #default="scope">
+          <span>{{ parseTime(scope.row.createTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="更新时间" align="center" prop="createTime" class-name="small-padding fixed-width"
+                       width="150px">
+        <template #default="scope">
+          <span>{{ parseTime(scope.row.updateTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="170" align="center" class-name="small-padding fixed-width" fixed="right"
+                       v-if="$slots.tableOperate">
+        <template #default="scope">
+          <slot name="tableOperate" :scope="scope"></slot>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+        v-show="total > 0 && showPagination"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
+    />
+
+  </div>
+</template>
+
+<script setup>
+import {listCase} from "@/api/hrm/case";
+import {selectModulList} from "@/api/hrm/module";
+import {listProject} from "@/api/hrm/project";
+import {HrmDataTypeEnum} from "@/components/hrm/enum.js";
+import DictTag from "@/components/DictTag/index.vue";
+// import JsonEditorVue from "json-editor-vue3";
+
+const {proxy} = getCurrentInstance();
+// const {sys_normal_disable} = proxy.useDict("sys_normal_disable");
+// const {hrm_data_type} = proxy.useDict("hrm_data_type");
+const {qtr_case_status} = proxy.useDict("qtr_case_status");
+
+const props = defineProps({
+  dataType: {type: Number, default: HrmDataTypeEnum.case},
+  showPagination: {type: Boolean, default: true},
+  checkedIds: {type: Array, default: []}
+});
+
+const emits = defineEmits(['selectChange']);
+defineExpose({handleQuery, getSelectedIds});
+
+
+/*
+* 区分用例还是配置
+* */
+const dataName = computed(() => {
+  return props.dataType === HrmDataTypeEnum.case ? "用例" : "配置";
+});
+
+// provide("hrm_data_type", hrm_data_type);
+// provide('sys_normal_disable', sys_normal_disable);
+provide('qtr_case_status', qtr_case_status);
+
+const tableRef = ref(null);
+const ruleList = ref([]);
+const projectOptions = ref([]);
+const moduleOptions = ref([]);
+const open = ref(false);
+// const loading = ref(true);
+const showSearch = ref(true);
+const allSelectIds = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+const onlySelf = ref(true);
+const _selectIds = ref(new Set());
+
+const queryParams = toRef({
+  pageNum: 1,
+  pageSize: 10,
+  type: props.dataType,
+  ruleId: undefined,
+  name: undefined,
+  path: undefined,
+  projectId: undefined,
+  moduleId: undefined,
+  status: undefined,
+  onlySelf: onlySelf
+});
+
+const loading = ref({
+  page: false,
+  edite: false,
+  run: false,
+  copy: false
+});
+
+/*
+* 显示真实名称还是“全局”，没有设置项目和模块的显示未全局
+* */
+function nameOrGlob(val) {
+  return val ? val : "全局";
+}
+
+/** 查询用例列表 */
+function getList() {
+  loading.value.page = true;
+  // console.log(tableRef.value.getSelectionRows());
+  listCase(queryParams.value).then(response => {
+    ruleList.value = response.rows;
+    total.value = response.total;
+    nextTick(() => {
+      // if (tableRef.value) {
+      //   tableRef.value.clearSelection();
+      // }
+
+      if (allSelectIds.value && allSelectIds.value.length > 0) {
+        if (tableRef.value && ruleList.value) {
+          ruleList.value.forEach(item => {
+            if (allSelectIds.value.some(dataId => dataId === item.ruleId)) {
+              tableRef.value.toggleRowSelection(item, true);
+            } else {
+              tableRef.value.toggleRowSelection(item, false);
+            }
+          });
+        }
+      }
+    });
+  }).finally(() => {
+    loading.value.page = false;
+  });
+}
+
+/** 查询项目列表 */
+function getProjectSelect() {
+  listProject({isPage: false}).then(response => {
+    projectOptions.value = response.data;
+  });
+}
+
+/** 查询模块列表 */
+function getModuleSelect() {
+  selectModulList(queryParams.value).then(response => {
+    moduleOptions.value = response.data;
+  });
+}
+
+/**重置查询条件所属模块下拉框*/
+function resetModule() {
+  queryParams.value.moduleId = undefined;
+  getModuleSelect();
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+function handleSelect(selection, row) {
+  let pageIds = ruleList.value.map(item => item.ruleId);
+  let pageSelectIds = selection.map(item => item.ruleId);
+  let delIds = pageIds.filter(item=> !pageSelectIds.includes(item));
+  pageSelectIds.forEach((dataId) => {
+    if (!allSelectIds.value.some((ruleId) => ruleId === dataId)) {
+        allSelectIds.value.push(dataId);
+      }
+  });
+  allSelectIds.value = allSelectIds.value.filter(item => !delIds.includes(item));
+}
+
+/** 多选框选中数据 */
+function handleSelectionChange(selection) {
+  // console.log(selection);
+  emits('selectChange', selection);
+  // let pageSelectIds = selection.map(item => item.ruleId);
+  // let pageIds = ruleList.value.map(item => item.ruleId);
+  // 从选中数据中排除当前页的数据
+  // allSelectIds.value = allSelectIds.value.filter((dataId) => !ruleList.value.some(item=>item.ruleId === dataId));
+
+  // 添加当前页选中的数据
+  // selection.forEach((item) =>{
+  //   if (!allSelectIds.value.some((ruleId)=>ruleId === item.ruleId)){
+  //     allSelectIds.value.push(item.ruleId);
+  //   }
+  // });
+
+  single.value = selection.length !== 1;
+  multiple.value = !selection.length;
+}
+
+function handleSelectAll(selection) {
+  let pageSelectIds = selection.map(item => item.ruleId);
+  if (pageSelectIds && pageSelectIds.length > 0) {
+    pageSelectIds.forEach((item) => {
+      if (!allSelectIds.value.some((ruleId) => ruleId === item)) {
+        allSelectIds.value.push(item);
+      }
+    });
+  } else {
+    let pageIds = ruleList.value.map(item => item.ruleId);
+    allSelectIds.value = allSelectIds.value.filter(item => !pageIds.includes(item));
+  }
+
+}
+
+function getSelectedIds() {
+  return allSelectIds.value;
+}
+
+watch(() => props.checkedIds, (newValue) => {
+  console.log("数据变化了");
+  // allSelectIds.value = newValue;
+}, {deep: true});
+
+
+onMounted(() => {
+  allSelectIds.value = toRaw(props.checkedIds);
+  // _selectIds.value.clear();
+  // props.checkedIds.forEach(item => _selectIds.value.add(item));
+  getProjectSelect();
+  getList();
+
+});
+
+</script>
---
+++ web/src/views/hrm/mock/index.vue
@@ -0,0 +1,323 @@
+<template>
+  <div class="app-container">
+    <MockTableQuery @select-change="handleSelectionChange" :data-type="dataType" ref="mockQueryViewRef">
+      <template #table-tool>
+        <el-col :span="1.5">
+          <el-button
+              type="primary"
+              plain
+              icon="Plus"
+              @click="handleAdd"
+              v-hasPermi="['hrm:case:add']"
+          >新增
+          </el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+              type="success"
+              plain
+              icon="Edit"
+              :disabled="single"
+              @click="handleUpdate"
+              v-hasPermi="['hrm:case:edit']"
+          >修改
+          </el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+              type="danger"
+              plain
+              icon="Delete"
+              :disabled="multiple"
+              @click="handleDelete"
+              v-hasPermi="['hrm:case:remove']"
+          >删除
+          </el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
+              type="warning"
+              plain
+              icon="Download"
+              @click="handleExport"
+              v-hasPermi="['hrm:case:export']"
+          >导出
+          </el-button>
+        </el-col>
+        <el-col :span="1.5">
+          <el-button type="warning"
+                     icon="CaretRight"
+                     @click="runTest"
+                     v-hasPermi="['hrm:case:run']"
+                     title="运行"
+                     v-if="dataType === HrmDataTypeEnum.case"
+                     :disabled="multiple"
+          >
+            执行
+          </el-button>
+        </el-col>
+      </template>
+      <template #caseStatus="{scope}">
+        <TagSelector v-model:selected-value="scope.row.status"
+                     :options="qtr_case_status"
+                     selector-width="90px"
+                     :source-data="scope.row"
+                     @selectChanged="lineStatusChange"
+        ></TagSelector>
+      </template>
+      <template #tableOperate="{scope}">
+        <el-button link type="primary" icon="Histogram" @click="showHistory(scope.row)"
+                   v-hasPermi="['hrm:case:history']"
+                   title="执行历史" v-if="dataType === HrmDataTypeEnum.case">
+        </el-button>
+        <el-button link type="warning" icon="Edit" :loading="loading.edite" @click="handleUpdate(scope.row)"
+                   v-hasPermi="['hrm:case:edit']" title="编辑">
+        </el-button>
+        <el-button link type="warning" icon="CaretRight" :loading="loading.run" @click="runTest(scope.row)"
+                   v-hasPermi="['hrm:case:run']" title="运行" v-if="dataType === HrmDataTypeEnum.case">
+        </el-button>
+        <el-button link type="warning" icon="CopyDocument" :loading="loading.copy" @click="showCopyDialog(scope.row)"
+                   v-hasPermi="['hrm:case:copy']" title="复制">
+        </el-button>
+        <el-button link type="danger" icon="Delete" @click="handleDelete(scope.row)"
+                   v-hasPermi="['hrm:case:remove']" title="删除">
+        </el-button>
+      </template>
+    </MockTableQuery>
+
+    <!-- 添加或修改用例对话框 -->
+    <CaseEditDialog :form-datas="form"
+                    :data-type="dataType"
+                    :form-rules="formRules"
+                    v-model:open-case-edit-dialog="open"
+                    :title=caseEditDialogTitle
+
+    ></CaseEditDialog>
+
+    <!-- 用例执行历史对话框 -->
+    <el-dialog fullscreen :title="'【' + currentCaseInfo?.caseId + '】' + currentCaseInfo?.caseName"
+               v-model="showHistoryDialog" append-to-body
+               v-if="dataType===HrmDataTypeEnum.case" destroy-on-close>
+      <el-container style="height: 100%">
+        <!--          <el-header height="20px" border="2px" style="border-bottom-color: #97a8be;text-align: right">-->
+        <!--          </el-header>-->
+        <el-main style="max-height: calc(100vh - 95px);">
+          <RunDetail :run-id="currentRunId" :view-type="runDetailViewTypeEnum.case"></RunDetail>
+        </el-main>
+      </el-container>
+    </el-dialog>
+
+    <!-- 复制用例对话框 -->
+    <el-dialog :title="copyCaseInfo?.caseName" v-model="copyDialog" append-to-body destroy-on-close>
+      <el-container style="height: 100%">
+        <!--          <el-header height="20px" border="2px" style="border-bottom-color: #97a8be;text-align: right">-->
+        <!--          </el-header>-->
+        <el-main style="max-height: calc(100vh - 95px);">
+          <el-input placeholder="请输入用例名称" v-model="copyCaseInfo.caseName">
+            <template #suffix>
+              <el-button @click="copyCaseHandle">保存</el-button>
+            </template>
+          </el-input>
+        </el-main>
+      </el-container>
+    </el-dialog>
+
+    <!-- 运行用例对话框 -->
+    <RunDialog v-model:dialog-visible="runDialogShow" :run-type="RunTypeEnum.case" :run-ids="runIds"></RunDialog>
+  </div>
+</template>
+
+<script setup>
+import {changeCaseStatus, copyCase, delCase, getCase, listCase} from "@/api/hrm/case";
+import TagSelector from "@/components/hrm/common/tag-selector.vue";
+import CaseEditDialog from "@/components/hrm/case/case-edit-dialog.vue"
+import {initCaseFormData} from "@/components/hrm/data-template.js";
+import RunDetail from '@/components/hrm/common/run/run-detail.vue';
+import RunDialog from '@/components/hrm/common/run/run_dialog.vue';
+import {HrmDataTypeEnum, runDetailViewTypeEnum, RunTypeEnum} from "@/components/hrm/enum.js";
+import {ElMessage, ElMessageBox} from "element-plus";
+import MockTableQuery from "@/components/hrm/util-data-table/mock-table-query.vue";
+// import JsonEditorVue from "json-editor-vue3";
+
+const {proxy} = getCurrentInstance();
+const {sys_normal_disable} = proxy.useDict("sys_normal_disable");
+const {hrm_data_type} = proxy.useDict("hrm_data_type");
+const {qtr_case_status} = proxy.useDict("qtr_case_status");
+
+const props = defineProps({
+  dataType: {type: Number, default: HrmDataTypeEnum.case},
+  formRules: {
+    type: Object,
+    default: {
+      caseName: [{required: true, message: "用例名称不能为空", trigger: "blur"}],
+      projectId: [{required: true, message: "所属项目不能为空", trigger: "blur"}],
+      moduleId: [{required: true, message: "所属模块不能为空", trigger: "blur"}]
+    }
+  }
+});
+
+const dataName = computed(() => {
+  return props.dataType === HrmDataTypeEnum.case ? "用例" : "配置";
+});
+
+provide("hrm_data_type", hrm_data_type);
+provide('sys_normal_disable', sys_normal_disable);
+provide('qtr_case_status', qtr_case_status);
+
+const mockQueryViewRef = ref(null);
+const projectOptions = ref([]);
+const open = ref(false);
+// const loading = ref(true);
+const showSearch = ref(true);
+const selectIds = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+const onlySelf = ref(true);
+
+const runDialogShow = ref(false);
+const runIds = ref([]);
+
+const showHistoryDialog = ref(false);
+const currentCaseInfo = ref(null);
+const currentRunId = ref();
+
+const copyDialog = ref(false);
+const copyCaseInfo = ref(initCaseFormData);
+
+const queryParams = toRef({
+  pageNum: 1,
+  pageSize: 10,
+  type: props.dataType,
+  caseId: undefined,
+  caseName: undefined,
+  projectId: undefined,
+  moduleId: undefined,
+  status: undefined,
+  onlySelf: onlySelf
+});
+
+const form = ref(initCaseFormData);
+const loading = ref({
+  page: false,
+  edite: false,
+  run: false,
+  copy: false
+});
+
+const caseEditDialogTitle = computed(() => {
+  const caseId = form.value.caseId ? '【' + form.value.caseId + '】' : "";
+  return title.value + '>> ' + caseId + form.value.caseName;
+});
+
+function lineStatusChange(selectValue, dataSource) {
+  changeCaseStatus({caseId: dataSource.caseId, status: selectValue}).then((response) => {
+    ElMessage.success("修改成功");
+  }).catch(() => {
+    ElMessage.error("修改失败");
+  });
+}
+
+/*
+* 行执行用例
+* **/
+function runTest(row) {
+  if (row && "caseId" in row && row.caseId) {
+    runIds.value = [row.caseId];
+  }else {
+    runIds.value = selectIds.value;
+  }
+
+  if (!runIds.value || runIds.value.length === 0) {
+    ElMessageBox.alert('请选择要运行的用例', "提示！", {type: "warning"});
+    return;
+  }
+  runDialogShow.value = true;
+}
+
+/*
+* 换起用例复制弹窗
+* */
+function showCopyDialog(data) {
+  copyDialog.value = true;
+  copyCaseInfo.value = structuredClone(toValue(toRaw(data)));
+}
+
+/*
+* 复制用例
+* */
+function copyCaseHandle() {
+  copyDialog.value = true;
+  let data = {
+    caseId: copyCaseInfo.value.caseId,
+    caseName: copyCaseInfo.value.caseName,
+  }
+  copyCase(data).then(response => {
+    ElMessage.success("复制成功");
+  }).finally(() => {
+    copyDialog.value = false;
+  });
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  title.value = "添加" + dataName.value;
+  form.value = JSON.parse(JSON.stringify(initCaseFormData));
+  open.value = true;
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  loading.value.edite = true;
+  const caseId = row.caseId || selectIds.value;
+  getCase(caseId).then((response) => {
+    if (!response.data || Object.keys(response.data).length === 0) {
+      ElMessage.warning("未查到对应数据！");
+      return;
+    }
+    form.value = response.data;
+
+    title.value = "修改" + dataName.value;
+    open.value = true;
+  }).finally(() => {
+    loading.value.edite = false;
+  });
+}
+
+function showHistory(row) {
+  const caseId = row.caseId || selectIds.value;
+  currentCaseInfo.value = row;
+  currentRunId.value = caseId;
+  showHistoryDialog.value = true;
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const caseIds = row.caseId || selectIds.value;
+  proxy.$modal.confirm('是否确认删除ID为"' + caseIds + '"的数据项？').then(function () {
+    return delCase(caseIds);
+  }).then(() => {
+    if (mockQueryViewRef.value){
+      mockQueryViewRef.value.handleQuery();
+    }
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => {
+  });
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download("hrm/case/export", {
+    ...queryParams.value
+  }, `Case_${new Date().getTime()}.xlsx`);
+}
+
+/** 多选框选中数据 */
+function handleSelectionChange(selection) {
+  selectIds.value = selection.map(item => item.caseId);
+  single.value = selection.length !== 1;
+  multiple.value = !selection.length;
+}
+
+</script>
