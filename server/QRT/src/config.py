adaad12f3ce0b4447ce92a1f68c65136b813b5d4
xiongjie <wwwxiongjie@gmail.com>
2025-08-28 02:37:09 +0800

    资源路径

--- server/QRT/src/config.py
+++ server/QRT/src/config.py
@@ -1,13 +1,23 @@
 import os
+import sys
+
+def resource_path(relative_path: str) -> str:
+    """获取资源文件绝对路径，兼容 PyInstaller 打包"""
+    if hasattr(sys, '_MEIPASS'):  # PyInstaller 打包后执行路径
+        return os.path.join(sys._MEIPASS, relative_path)
+    # 正常开发环境
+    return os.path.join(os.path.dirname(__file__), "..", relative_path)
 
 class AppConfig:
     """配置应用基本信息"""
 
     def __init__(self, ft, page):
+        config_path = resource_path("tools_config.json")
+        print(config_path)
         self.page = page
         self.page.title = "TestTools"
         self.version = "1.0.0.0"
-        self.tools_db = "tools_config.json"
+        self.tools_db = config_path
         self.page.theme_mode = ft.ThemeMode.LIGHT # 亮色主题
         self.page.theme = ft.Theme(color_scheme_seed=ft.Colors.GREEN)
         # 基础窗口配置
