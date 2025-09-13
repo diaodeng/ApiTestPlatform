fd38553c9fb22c2ed07cc53f712e98034edc7ab5
jie.xiong <jie.xiong@dmall.com>
2025-09-13 17:46:01 +0800

    other:增加客户端

---
+++ client/src/utils/mitmproxy_tool.py
@@ -0,0 +1,337 @@
+import asyncio
+import os
+import sys
+import socket
+import platform
+import re
+import json
+import threading
+from collections import defaultdict
+import requests
+
+
+import psutil
+# import psutil
+from mitmproxy.options import Options
+from mitmproxy.tools.web.master import WebMaster, webaddons
+from mitmproxy.tools.dump import DumpMaster
+from mitmproxy.http import HTTPFlow, Response
+from loguru import logger
+
+from server.config import MitmproxyConfig
+
+
+def get_local_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
+    except Exception as e:
+        return f"无法获取 IP: {e}"
+
+
+def get_all_macs():
+    system = platform.system().lower()
+    if system == "windows":
+        cmd = "getmac"
+    else:
+        cmd = "ifconfig -a"
+
+    output = os.popen(cmd).read()
+
+    macs = re.findall(r"([0-9A-Fa-f]{2}(?:[:-][0-9A-Fa-f]{2}){5})", output)
+    return ",".join([i.replace("-", "").lower() for i in set(macs) if i != "00-00-00-00-00-00"])
+
+is_mock2 = False
+
+class MockHandle:
+    count = 0
+    local_ip = get_local_ip()
+    local_mac = get_all_macs()
+
+    mock_server = "https://testautoapi.rta-os.com/hrm/mock"
+    open_include = False
+    open_exclude = False
+    include = []
+    exclude = []
+    is_mock = True
+    add_headers = dict()
+    add_body = dict()
+    def __init__(self):
+        # self.client = httpx.AsyncClient(timeout=5)
+        config = MitmproxyConfig.read()
+
+        if config.add_headers:
+            lines = config.add_headers.split("\n")
+            for line in lines:
+                line = line.strip().split("=", 1)
+                if len(line) == 2:
+                    MockHandle.add_headers[line[0]] = line[1]
+        if config.add_body:
+            lines = config.add_body.split("\n")
+            for line in lines:
+                line = line.strip().split("=", 1)
+                if len(line) == 2:
+                    MockHandle.add_body[line[0]] = line[1]
+
+
+        MockHandle.is_mock = config.is_mock
+        MockHandle.open_include = config.open_include
+        MockHandle.open_exclude = config.open_exclude
+        MockHandle.exclude = config.exclude.split(",")
+        MockHandle.include = config.include.split(",")
+        MockHandle.mock_server = config.mock_server
+
+
+    async def request(self, flow: HTTPFlow):
+        logger.info(f"request: {flow.request.path}")
+        logger.info(f"is_mock: {MockHandle.is_mock}, include: {MockHandle.include}, exclude: {MockHandle.exclude}")
+
+        if not MockHandle.is_mock:
+            logger.info(f"not mock: {flow.request.path}")
+            return
+
+        if MockHandle.open_include and flow.request.path not in MockHandle.include:
+            logger.info(f"not include: {flow.request.path}")
+            return
+
+        if MockHandle.open_exclude and flow.request.path in MockHandle.exclude:
+            logger.info(f"exclude: {flow.request.path}")
+            return
+
+        try:
+            try:
+                json_data = flow.request.json()
+            except Exception as e:
+                json_data = None
+            logger.info(f"path: {flow.request.path}")
+
+            headers = {
+                "ip": MockHandle.local_ip,
+                "mac": MockHandle.local_mac,
+                **MockHandle.add_headers
+            }
+            result_data = requests.post(f"{MockHandle.mock_server}{flow.request.path}",
+                                    json=json_data,
+                                    data=dict(flow.request.urlencoded_form),
+                                    headers=headers,
+                                    timeout=3
+                                    )
+            if result_data.status_code == 200:
+                res_data = result_data.json()
+                logger.info(res_data)
+                flow.response = Response.make(200,
+                                              content=json.dumps(res_data).encode('utf-8'),
+                                              headers={"Content-Type": "application/json"},
+                                              )
+                return
+            logger.info(f"mock error[{flow.request.path}]: {result_data.status_code}")
+        except Exception as e:
+            logger.exception(e)
+            logger.error(f"request mock error[{flow.request.path}]: {e}")
+
+    async def response(self, flow: HTTPFlow):
+        pass
+
+
+class ProxyCore:
+    def __init__(self, config_data=None):
+        self.master: WebMaster = None
+        self.loop: asyncio.AbstractEventLoop = None
+        self.task: asyncio.Task = None
+        self.process: threading.Thread = None
+        self.old_process_info = defaultdict()
+
+    @classmethod
+    async def update_config(cls):
+        config = MitmproxyConfig.read()
+
+        if config.add_headers:
+            lines = config.add_headers.split("\n")
+            for line in lines:
+                line = line.strip().split("=", 1)
+                if len(line) == 2:
+                    MockHandle.add_headers[line[0]] = line[1]
+        if config.add_body:
+            lines = config.add_body.split("\n")
+            for line in lines:
+                line = line.strip().split("=", 1)
+                if len(line) == 2:
+                    MockHandle.add_body[line[0]] = line[1]
+
+        MockHandle.is_mock = config.is_mock
+        MockHandle.open_include = config.open_include
+        MockHandle.open_exclude = config.open_exclude
+        MockHandle.exclude = config.exclude.split(",")
+        MockHandle.include = config.include.split(",")
+        MockHandle.mock_server = config.mock_server
+
+    async def run(self,
+                  port=8080,
+                  web_port=8081,
+                  web_open_browser=True,
+                  config_dir="",
+                  loop=None,
+                  mode=["local:CPOS-DF,df_sv,Launcher,java,Pos,CPOS-KH,ONENOTE,ONENOTEM,HttpServer"]):
+        """
+        web-port
+        """
+        logger.info("mitmproxy proxy start")
+        try:
+            if config_dir and not os.path.exists(config_dir):
+                os.makedirs(config_dir)
+            opts = Options(listen_host="127.0.0.1",
+                           listen_port=port,
+                           confdir=config_dir or os.path.join(os.path.expanduser("~"), ".mitmproxy"),
+                           # confdir="C:\\Users\\Administrator\\.mitmproxy",
+                           mode=mode,
+                           )
+
+            # self.master = DumpMaster(opts, with_termlog=False, with_dumper=False)
+
+            self.master = WebMaster(opts, with_termlog=True)
+
+            self.master.addons.add(MockHandle())
+            self.master.options.add_option("web_host", str, "127.0.0.1", "")
+            self.master.options.add_option("web_port", int, web_port, "")
+            self.master.options.add_option("web_open_browser", bool, web_open_browser, "")
+        except Exception as e:
+            logger.error(f"mitmproxy proxy error[{e}]")
+            logger.exception(e)
+
+        logger.info("mitmproxy proxy start ...")
+        try:
+            await self.master.run()
+            # self.master.running()
+        except KeyboardInterrupt:
+            logger.info("mitmproxy stopped")
+            self.master.shutdown()
+
+
+    def start_loop(self, port, web_port, web_open_browser, config_dir=None):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(self.run(port=port, web_port=web_port, web_open_browser=web_open_browser, config_dir=config_dir, loop=self.loop))
+
+
+    def start(self, port=8080, web_port=8081, open_browser=True, config_dir=None):
+        if self.process and self.process.is_alive():
+            print("mitmproxy already running")
+            return
+        self.process = threading.Thread(
+            target=self.start_loop, args=(port, web_port, open_browser, config_dir), daemon=True
+        )
+        self.process.start()
+        print(f"mitmproxy started pid={self.process.name}")
+
+    def stop(self):
+
+        if self.master:
+            # self.master.event_loop.stop()
+            self.master.shutdown()
+
+            logger.info(f"mitmproxy stopped")
+
+        if self.task:
+            try:
+                self.task.cancel()
+                self.task.done()
+                # self.task.exception()
+
+                logger.info(f"mitmproxy task cancelled")
+            except Exception as e:
+                logger.error(f"mitmproxy stop task error[{e}]")
+
+        if self.loop:
+            try:
+                self.loop.stop()
+                # self.loop.close()
+
+                logger.info(f"mitmproxy loop stopped")
+            except Exception as e:
+                logger.error(f"mitmproxy stop loop error[{e}]")
+
+        if self.process:
+            try:
+                self.process.join(timeout=2)
+
+                logger.info(f"mitmproxy process stopped")
+            except Exception as e:
+                logger.error(f"mitmproxy stop thread error[{e}]")
+
+        self.master = None
+        self.task = None
+        self.loop = None
+        self.process = None
+
+    def open_url(self):
+        if self.master and isinstance(self.master, WebMaster):
+            webaddons.open_browser(self.master.web_url)
+        else:
+            logger.info(f"只能在web模式下才能打开浏览器，{type(self.master)}")
+
+
+    @classmethod
+    def find_mitm_children(cls):
+        p = psutil.Process(os.getpid())
+
+        # 获取所有子孙进程（递归）
+        descendants = p.children(recursive=True)
+        for d in descendants:
+            logger.info(f"后代 PID={d.pid}, 名称={d.name()}， 状态={d.status()}， 信息：{d.exe()}")
+        logger.info(f"mitmproxy 后代进程获取完毕")
+        return
+
+        current_pid = os.getpid()
+        children = []
+        threads = []
+        # 查找所有子进程
+        for proc in psutil.process_iter(['pid', 'ppid', 'name', "exe"]):
+            try:
+                logger.debug(f"进程： {proc.info['name']}")
+                if "mitm" in proc.info["name"].lower():
+                    logger.info(f"这是啥：{proc}")
+
+                if proc.info['ppid'] == current_pid:
+                    # 查找当前进程的所有线程
+                    parent = psutil.Process(current_pid)
+                    threads.extend(parent.threads())
+                    logger.info(proc)
+                    logger.info(f"{proc.info['pid']}  mitmproxy threads: {[thread.id for thread in threads]}")
+
+                    # if proc.info['name'] != "flet.exe":
+                    #     proc.terminate()
+                    # else:
+                    #     pass
+
+                    # 查找当前进程的所有线程
+                    # for thread in threads:
+                    #     logger.info(f"{current_pid} {thread}")
+                    #     logger.debug(f"{dir(thread)}")
+
+                    # if proc.info['name'] != "flet.exe":
+                    #     proc.terminate()
+                        # children.append(proc)
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                continue
+
+        return children, threads
+
+
+    @classmethod
+    def config_path(cls):
+        return "mitmproxy"
+
+
+if __name__ == "__main__":
+    server_port = 8080
+    web_prot = 8081
+    if len(sys.argv) > 1:
+        server_port = int(sys.argv[1])
+        web_prot = int(sys.argv[2])
+    proxy = ProxyCore()
+    proxy.start_loop(server_port, web_prot)
+
---
+++ client/src/view_contents/exitAlertDialog.py
@@ -0,0 +1,35 @@
+import sys
+import threading
+
+from utils.mytimers import clear_all_timers
+from flet import Page
+
+class ExitAlertDialog:
+    def __init__(self, ft, page, **kwargs):
+        self.ft = ft
+        self.page: Page = page
+        self.kwargs = kwargs
+
+        self.confirm_dialog = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Do you really want to exit this app?"),
+            # content=ft.Text("Do you really want to exit this app?"),
+            actions=[
+                ft.ElevatedButton("Yes", on_click=self.yes_click),
+                ft.OutlinedButton("No", on_click=self.no_click),
+            ],
+            actions_alignment=ft.MainAxisAlignment.END,
+        )
+        page.update()
+
+    def yes_click(self, e):
+        self.page.close(self.confirm_dialog)
+        self.page.update()
+        # 页面包含定时器，需要先关闭定时器，清理资源
+        clear_all_timers()
+        self.page.window.destroy()
+        # sys.exit(0)  # 直接退出进程
+
+    def no_click(self, e):
+        self.page.close(self.confirm_dialog)
+        self.page.update()
