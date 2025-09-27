a411ba4cc3112a91a626a83b7047f58af806e0a1
jie.xiong <jie.xiong@dmall.com>
2025-09-27 14:24:07 +0800

    others: 日志优化，依赖更新

--- server/utils/log_util.py
+++ server/utils/log_util.py
@@ -7,6 +7,8 @@ import datetime
 
 from loguru import logger
 
+current_file_size = 0
+max_file_size = 1024 * 1024 * 1024
 log_dir = os.path.join(os.getcwd(), 'logs')
 if not os.path.exists(log_dir):
     os.mkdir(log_dir)
@@ -17,18 +19,32 @@ def request_id():
     except:
         return ''
 
+def rotation_handler(message, file):
+    global current_file_size
+    # current_file_size += len(message.record["message"].encode("utf-8"))
+    current_file_size = os.path.getsize(file.name)
+    if current_file_size >= max_file_size:
+        current_file_size = 0
+        return True
+    elif message.record["time"].strftime("%Y-%m-%d") != time.strftime("%Y-%m-%d"):
+        current_file_size = 0
+        return True
+    else:
+        return False
+
+
 log_path = os.path.join(log_dir, f'{time.strftime("%Y-%m-%d")}.log')
 log_path_error = os.path.join(log_dir, f'error_{time.strftime("%Y-%m-%d")}.log')
 log_path_mock = os.path.join(log_dir, f'mock_{time.strftime("%Y-%m-%d")}_test.log')
 
 logger.remove()
 logger.add(sys.stderr)
-logger.add(log_path, rotation=datetime.timedelta(days=1), encoding="utf-8", enqueue=True, filter=lambda record: record["extra"].get("name", "") != "mock_request")
-logger.add(log_path_error, rotation=datetime.timedelta(days=1), encoding="utf-8", enqueue=True, filter=lambda record: record["level"].no >= logging.ERROR)
+logger.add(log_path, rotation=rotation_handler, encoding="utf-8", enqueue=True, filter=lambda record: record["extra"].get("name", "") != "mock_request")
+logger.add(log_path_error, rotation=rotation_handler, encoding="utf-8", enqueue=True, filter=lambda record: record["level"].no >= logging.ERROR)
 
 
 logger_mock = logger.bind(name="mock_request")
-logger_mock.add(log_path_mock, rotation=datetime.timedelta(days=1), encoding="utf-8", enqueue=True, filter=lambda record: record["extra"].get("name", "") == "mock_request")
+logger_mock.add(log_path_mock, rotation=rotation_handler, encoding="utf-8", enqueue=True, filter=lambda record: record["extra"].get("name", "") == "mock_request")
 
 
 
