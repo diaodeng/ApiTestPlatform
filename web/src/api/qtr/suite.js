32785219c89c7ef70548c462dd2748242250173a
panda <wwwxiongjie@126.com>
2026-01-30 14:40:35 +0800

    others: 实现套件管理无法显示项目，无法删除详情

--- web/src/api/qtr/suite.js
+++ web/src/api/qtr/suite.js
@@ -2,89 +2,100 @@ import request from '@/utils/request'
 
 // 查询测试套件列表
 export function listSuite(query) {
-  return request({
-    url: '/qtr/suite/list',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: '/qtr/suite/list',
+        method: 'get',
+        params: query
+    })
 }
 
 // 查询测试套件详细
 export function getSuite(suiteId) {
-  return request({
-    url: '/qtr/suite/' + suiteId,
-    method: 'get'
-  })
+    return request({
+        url: '/qtr/suite',
+        method: 'get',
+        params: {"suiteId": suiteId}
+    })
 }
 
 // 新增测试套件
 export function addSuite(data) {
-  return request({
-    url: '/qtr/suite',
-    method: 'post',
-    data: data
-  })
+    return request({
+        url: '/qtr/suite',
+        method: 'post',
+        data: data
+    })
 }
 
 // 修改测试套件
 export function updateSuite(data) {
-  return request({
-    url: '/qtr/suite',
-    method: 'put',
-    data: data
-  })
+    return request({
+        url: '/qtr/suite',
+        method: 'put',
+        data: data
+    })
 }
 
 // 删除测试套件
 export function delSuite(suiteId) {
-  return request({
-    url: '/qtr/suite/' + suiteId,
-    method: 'delete'
-  })
+    return request({
+        url: '/qtr/suite',
+        method: 'delete',
+        data: {"suiteIds": suiteId}
+    })
 }
 
 /**************测试套件详细**************/
 //查询测试套件详细列表
-export function listDetailSuite(query){
-  return request({
-    url: '/qtr/suite/detailList',
-    method: 'get',
-    params: query
-  })
+export function listDetailSuite(query) {
+    return request({
+        url: '/qtr/suite/detailList',
+        method: 'get',
+        params: query
+    })
 }
 
 // 新增测试套件详情
 export function addSuiteDetail(data) {
-  return request({
-    url: '/qtr/suite/addSuiteDetail',
-    method: 'post',
-    data: data
-  })
+    return request({
+        url: '/qtr/suite/addSuiteDetail',
+        method: 'post',
+        data: data
+    })
 }
 
 // 修改测试套件详细
 export function updateSuiteDetail(data) {
-  return request({
-    url: '/qtr/suite/editSuiteDetail',
-    method: 'put',
-    data: data
-  })
+    return request({
+        url: '/qtr/suite/editSuiteDetail',
+        method: 'put',
+        data: data
+    })
 }
 
 // 查询测试套件详细
 export function getSuiteDetail(suiteDetailId) {
-  return request({
-    url: '/qtr/suite/getSuiteDetail/' + suiteDetailId,
-    method: 'get'
-  })
+    return request({
+        url: '/qtr/suite/getSuiteDetail/' + suiteDetailId,
+        method: 'get'
+    })
 }
 
 
 // 修改套件内数据的顺序
 export function changeContentOrder(data) {
-  return request({
-    url: '/qtr/suite/changeOrder',
-    method: 'post',
-    data: data
-  })
+    return request({
+        url: '/qtr/suite/changeOrder',
+        method: 'post',
+        data: data
+    })
+}
+
+//删除套件详情
+export function delSuiteDetailRequest(data) {
+    return request({
+        url: '/qtr/suite/detail',
+        method: 'delete',
+        data: data
+    })
 }
