5fb74c4d09137d3657233d1ee7adea48a3a79f2a
panda <wwwxiongjie@126.com>
2026-02-11 12:28:33 +0800

    others: 脚本使用uv安装依赖，requirements.txt留空

--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+export PATH="/usr/local/bin:/app/.venv/bin:$PATH"
 
 echo "开始安装系统依赖。。。"
 sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list && \
@@ -9,11 +11,16 @@ sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list && \
 
 echo "系统依赖安装完成。。。"
 
+echo "开始安装 uv..."
+/usr/local/bin/pip3 install -U uv -i https://pypi.tuna.tsinghua.edu.cn/simple
+echo "uv 安装完成"
+
 echo "开始安装应用依赖。。。"
 # 安装依赖
-/usr/local/bin/pip3 install -r /app/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+#/usr/local/bin/pip3 install -r /app/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+cd /app && python -m uv sync
 echo "应用依赖安装完成。。。"
 
 # 启动fastapi应用
 echo "开始启动。。。"
-python app.py --env=prod
+/app/.venv/bin/python app.py --env=prod
