f482eee0f5fbe77c9a1afc8e9c69c54a66e1d464
panda <wwwxiongjie@126.com>
2026-02-26 10:32:01 +0800

    ohters: 前端地址恢复配置文件

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,4 @@
-window.BASE_API = window.__APP_CONFIG__?.BASE_API || window.BASE_API || "http://localhost:80";
+window.BASE_API = window.__APP_CONFIG__?.BASE_API || import.meta.env.VITE_APP_BASE_API || "http://localhost:80";
 import { createApp } from 'vue'
 
 import Cookies from 'js-cookie'
