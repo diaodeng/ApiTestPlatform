841665cede771048315967d00c4c9a3119ac956e
panda <wwwxiongjie@126.com>
2026-03-06 13:36:47 +0800

    others: diff增加清空按钮

--- web/src/views/hrm/tools/diff.vue
+++ web/src/views/hrm/tools/diff.vue
@@ -1,10 +1,23 @@
 <script lang="ts" setup>
 import DiffEditor from "@/components/hrm/common/ace-editor-plug/DiffEditor.vue"
+import {ref} from "vue";
+
+const diffInstance = ref(null);
+
+function clear() {
+  diffInstance.value.clearContent();
+}
 
 </script>
 
 <template>
-    <DiffEditor></DiffEditor>
+  <div class="app-container">
+    <div class="diff-header">
+      <el-button @click="clear">清空</el-button>
+    </div>
+    <DiffEditor ref="diffInstance"></DiffEditor>
+  </div>
+
 </template>
 
 <style scoped lang="scss">
