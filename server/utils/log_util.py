665ff075d76bf2e241421cf6971949f645cd8f07
panda <wwwxiongjie@126.com>
2026-03-31 18:47:26 +0800

    fix: 日志分割问题处理

--- server/utils/log_util.py
+++ server/utils/log_util.py
@@ -1,8 +1,6 @@
-import asyncio
 import logging
 import os
 import sys
-import time
 from os import environ
 
 from loguru import logger
@@ -16,39 +14,37 @@ env_debug = environ.get("QTRDEBUG", "false").lower() == "true"
 log_to_console = environ.get("LOG_TO_CONSOLE", "false").lower() == "true"
 level_name = "DEBUG" if env_debug else "INFO"
 logger.level(level_name)
-def request_id():
-    try:
-        return asyncio.current_task().get_name()
-    except:
-        return ''
-
-def rotation_handler(message, file):
-    global current_file_size
-    # current_file_size += len(message.record["message"].encode("utf-8"))
-    current_file_size = os.path.getsize(file.name)
-    if current_file_size >= max_file_size:
-        current_file_size = 0
-        return True
-    elif message.record["time"].strftime("%Y-%m-%d") != time.strftime("%Y-%m-%d"):
-        current_file_size = 0
-        return True
-    else:
-        return False
-
-
-log_path = os.path.join(log_dir, f'{time.strftime("%Y-%m-%d")}.log')
-log_path_error = os.path.join(log_dir, f'error_{time.strftime("%Y-%m-%d")}.log')
-log_path_mock = os.path.join(log_dir, f'mock_{time.strftime("%Y-%m-%d")}_test.log')
 
 logger.remove()
 if log_to_console:
     logger.add(sys.stderr)
-logger.add(log_path, rotation=rotation_handler, encoding="utf-8", enqueue=True, filter=lambda record: record["extra"].get("name", "") != "mock_request")
-logger.add(log_path_error, rotation=rotation_handler, encoding="utf-8", enqueue=True, filter=lambda record: record["level"].no >= logging.ERROR)
+logger.add(
+    os.path.join(log_dir, "{time:YYYY-MM-DD}.log"),
+    rotation="00:00",
+    encoding="utf-8",
+    retention="7 days",
+    enqueue=True,
+    filter=lambda record: record["extra"].get("name", "") != "mock_request",
+)
+logger.add(
+    os.path.join(log_dir, "error_{time:YYYY-MM-DD}.log"),
+    rotation="00:00",
+    encoding="utf-8",
+    retention="30 days",
+    enqueue=True,
+    filter=lambda record: record["level"].no >= logging.ERROR,
+)
 
 
 logger_mock = logger.bind(name="mock_request")
-logger_mock.add(log_path_mock, rotation=rotation_handler, encoding="utf-8", enqueue=True, filter=lambda record: record["extra"].get("name", "") == "mock_request")
+logger_mock.add(
+    os.path.join(log_dir, "mock_{time:YYYY-MM-DD}.log"),
+    rotation="00:00",
+    encoding="utf-8",
+    retention="7 days",
+    enqueue=True,
+    filter=lambda record: record["extra"].get("name", "") == "mock_request",
+)
 
 
 # 拦截标准 logging 日志，交给 loguru
